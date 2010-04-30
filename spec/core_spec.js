797084d7623ef18e6a5b9cc64ffca38e17bebf32
Richard Ramsden <rramsden@bravenet.com>
2010-04-30 16:59:45 -0700

    added screw-unit test suite

---
+++ spec/core_spec.js
@@ -0,0 +1,12 @@
+Screw.Unit(function() {
+  describe("Creating a new game", function() {
+    it("browser should support HTML5", function() {
+    });
+
+    it("should create a new canvas element", function() {
+    });
+
+    it("should call the update loop", function() {
+    });
+  });
+});
