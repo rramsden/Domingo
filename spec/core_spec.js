c6b026ccc8c0523271bb933773f3b8bbd1035f06
Richard Ramsden <rramsden@bravenet.com>
2010-04-30 19:11:22 -0700

    added network stubs

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -1,6 +1,6 @@
 Screw.Unit(function() {
   describe("Creating a new game", function() {
-    it("browser should support HTML5", function() {
+    it("should check browser for HTML5 support", function() {
     });
 
     it("should create a new canvas element", function() {
